e7b1e8efae337bf5c92cca118d8a41c020182bb0
ImpanaD142 <2022cs_impanad_b@nie.ac.in>
2025-05-07 20:28:20 +0530

    Update App.js

--- social-media-analytics/frontend/src/App.js
+++ social-media-analytics/frontend/src/App.js
@@ -1,1 +1,21 @@
+javascript
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import TopUsers from './components/TopUsers';
+import TrendingPosts from './components/TrendingPosts';
+import Feed from './components/Feed';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<TopUsers />} />
+        <Route path="/trending" element={<TrendingPosts />} />
+        <Route path="/feed" element={<Feed />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
 
