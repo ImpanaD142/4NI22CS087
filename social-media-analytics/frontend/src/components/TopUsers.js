e48b89efd64be6b811708096914676b871ddb9a3
ImpanaD142 <2022cs_impanad_b@nie.ac.in>
2025-05-07 20:29:01 +0530

    Update TopUsers.js

--- social-media-analytics/frontend/src/components/TopUsers.js
+++ social-media-analytics/frontend/src/components/TopUsers.js
@@ -1,1 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Container, Typography, List, ListItem, ListItemText } from '@mui/material';
+
+const TopUsers = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/users');
+        setUsers(response.data);
+      } catch (error) {
+        console.error('Error fetching top users:', error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  return (
+    <Container>
+      <Typography variant="h4" gutterBottom>
+        Top Users
+      </Typography>
+      <List>
+        {users.map((user) => (
+          <ListItem key={user.id}>
+            <ListItemText primary={${user.name} - ${user.comments} comments} />
+          </ListItem>
+        ))}
+      </List>
+    </Container>
+  );
 
